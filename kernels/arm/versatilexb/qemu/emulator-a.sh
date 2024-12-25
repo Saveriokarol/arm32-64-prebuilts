bd26fb486e81e73b370c5387a74898c0a17b7531
Saveriokarol <saveriokarol21@gmail.com>
2024-12-25 16:25:48 +0100

    Update versatilexb

---
+++ kernels/arm/versatilexb/qemu/emulator-a.sh
@@ -0,0 +1,1 @@
+qemu-system-arm -M versatileab -kernel ../zImage -dtb ../versatile-ab.dtb -append "console=ttyAMA0,115200" $@
---
+++ kernels/arm/versatilexb/qemu/emulator-p.sh
@@ -0,0 +1,1 @@
+qemu-system-arm -M versatilepb -kernel ../zImage -dtb ../versatile-pb.dtb -append "console=ttyAMA0,115200" $@
